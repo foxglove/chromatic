ba2835a18f5b04938d9aa15b36e0382117690e2a
Devon Rueckner <devon@foxglove.dev>
2024-07-30 23:16:50 -0400

    add dist

---
+++ dist/node.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var chunkPFP7BIW5_js = require('./chunk-PFP7BIW5.js');
+require('./chunk-AXECKMHC.js');
+
+chunkPFP7BIW5_js.x();
+
+Object.defineProperty(exports, 'getConfiguration', {
+	enumerable: true,
+	get: function () { return chunkPFP7BIW5_js.l; }
+});
+Object.defineProperty(exports, 'getGitInfo', {
+	enumerable: true,
+	get: function () { return chunkPFP7BIW5_js.w; }
+});
+Object.defineProperty(exports, 'run', {
+	enumerable: true,
+	get: function () { return chunkPFP7BIW5_js.u; }
+});
+Object.defineProperty(exports, 'runAll', {
+	enumerable: true,
+	get: function () { return chunkPFP7BIW5_js.v; }
+});
